68ae67fe3af865b12e434ea3dbd79b4313febcbd
Quinn Munba <44347302+dailyquinn@users.noreply.github.com>
2024-11-24 22:38:02 -0600

    Update sysinfo_script.sh

--- sysinfo_script.sh
+++ sysinfo_script.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+echo "Hello $USER. System specifications are as follows:"
+echo ""
+head -n1 /etc/issue
+echo ""
+uname -a
+echo ""
+grep "model name" /proc/cpuinfo | sort -u
+grep MemTotal /proc/meminfo
+echo ""
+lsblk -o path,vendor,model,size,mountpoints,fsused,fsavail,fsuse%
